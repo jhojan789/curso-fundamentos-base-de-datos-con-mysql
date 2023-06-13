b396a1454ecff0f4fa7f6b227fc46a5e6be85b18
Jhojan Tobón <jhojantobonm@gmail.com>
2023-06-13 18:49:11 -0500

    SELECT all users that have no posts

--- DML/FROM/2-FROM.sql
+++ DML/FROM/2-FROM.sql
@@ -1,3 +1,8 @@
 SELECT *
 FROM usuarios
 	LEFT JOIN posts ON usuarios.id = posts.usuario_id;
+    
+SELECT *
+FROM usuarios
+	LEFT JOIN posts ON usuarios.id = posts.usuario_id
+WHERE posts.usuario_id IS NULL;
