49b8df1880862590b40cae6ee38ab9411680eff8
Jhojan Tobón <jhojantobonm@gmail.com>
2023-06-14 17:39:30 -0500

    added RIGHT JOIN keyword

--- DML/FROM/2-FROM.sql
+++ DML/FROM/2-FROM.sql
@@ -1,8 +1,35 @@
+
+-- trae todos los A sin importar que tengan o no tengan B asociados 
+-- y trae solo los B que tengan A asociados.
+-- (TRAE TODOS LOS USUARIOS SIN IMPORTAR QUE TENGAN O NO TENGAN POSTS ASOCIADOS 
+-- Y TRAE SOLO LOS POSTS QUE TENGAN USUARIOS ASOCIADOS)
+
 SELECT *
 FROM usuarios
 	LEFT JOIN posts ON usuarios.id = posts.usuario_id;
+    
+-- trae solo los A que no tengan B
+-- (TRAE SOLO LOS USUARIOS QUE NO TENGAN POSTS)
     
 SELECT *
 FROM usuarios
 	LEFT JOIN posts ON usuarios.id = posts.usuario_id
 WHERE posts.usuario_id IS NULL;
+
+-- trae todos los B sin importar que tengan o no tengan A asociados 
+-- y trae los A que tengan B asociados.
+-- (TRAE TODOS LOS POSTS SIN IMPORTAR QUE TENGAN O NO TENGAN USUARIOS ASOCIADOS 
+-- Y TRAE LOS USUARIOS QUE SÍ ESTÁN ASOCIADOS A UN POST)
+
+SELECT *
+FROM usuarios
+	RIGHT JOIN posts ON usuarios.id = posts.usuario_id;
+
+
+-- trae solo los B que no tengan A
+-- (TRAE SOLO LOS POSTS QUE NO TENGAN USUARIO)
+SELECT *
+FROM usuarios
+	RIGHT JOIN posts ON usuarios.id = posts.usuario_id
+WHERE posts.usuario_id IS NULL;
+
